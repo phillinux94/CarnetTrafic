ce4941b709be08dc3379dae59506e18777941e80
phillinux94 <phillinux94@gmail.com>
2020-12-15 23:50:42 +0100

    Interface graphique

--- src/com/phillinux94/carnettrafic/Controller.java
+++ src/com/phillinux94/carnettrafic/Controller.java
@@ -1,6 +1,8 @@
 package com.phillinux94.carnettrafic;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Pane;
 
 public class Controller {
@@ -15,11 +17,51 @@ public class Controller {
     private Pane selBtnCalculDistance;
 
     @FXML
+    private TitledPane paneCarnetTrafic;
+
+    @FXML
+    private TitledPane paneCalculLocator;
+
+    @FXML
+    private TitledPane paneCalculDistance;
+
+    // Coordonnées de la station
+    @FXML
+    private TextField fldIndicatifStation;
+
+    @FXML
+    private TextField fldLatitudeDegres;
+
+    @FXML
+    private TextField fldLatitudeMinutes;
+
+    @FXML
+    private TextField fldLatitudeSecondes;
+
+    @FXML
+    private TextField fldLongitudeDegres;
+
+    @FXML
+    private TextField fldLongitudeMinutes;
+
+    @FXML
+    private TextField fldLongitudeSecondes;
+
+    @FXML
+    private TextField fldLocatorStation;
+
+
+
+    @FXML
     private void setCarnetTrafic(){
 
         selBtnCarnetTrafic.setVisible(true);
         selBtnCalculLocator.setVisible(false);
         selBtnCalculDistance.setVisible(false);
+
+        paneCarnetTrafic.setVisible(true);
+        paneCalculLocator.setVisible(false);
+        paneCalculDistance.setVisible(false);
 
     }
 
@@ -30,6 +72,10 @@ public class Controller {
         selBtnCalculLocator.setVisible(true);
         selBtnCalculDistance.setVisible(false);
 
+        paneCarnetTrafic.setVisible(false);
+        paneCalculLocator.setVisible(true);
+        paneCalculDistance.setVisible(false);
+
     }
 
     @FXML
@@ -39,5 +85,15 @@ public class Controller {
         selBtnCalculLocator.setVisible(false);
         selBtnCalculDistance.setVisible(true);
 
+        paneCarnetTrafic.setVisible(false);
+        paneCalculLocator.setVisible(false);
+        paneCalculDistance.setVisible(true);
+
+    }
+
+    @FXML
+    private void calculLocatorLocal(){
+
+        fldLocatorStation.setText("");
     }
 }

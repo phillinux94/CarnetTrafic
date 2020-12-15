b7b875c3d3be615c46274f361081f946ec47fe83
phillinux94 <phillinux94@gmail.com>
2020-12-15 22:55:50 +0100

    Interface graphique

--- src/com/phillinux94/carnettrafic/Controller.java
+++ src/com/phillinux94/carnettrafic/Controller.java
@@ -1,4 +1,43 @@
 package com.phillinux94.carnettrafic;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+
+    @FXML
+    private Pane selBtnCarnetTrafic;
+
+    @FXML
+    private Pane selBtnCalculLocator;
+
+    @FXML
+    private Pane selBtnCalculDistance;
+
+    @FXML
+    private void setCarnetTrafic(){
+
+        selBtnCarnetTrafic.setVisible(true);
+        selBtnCalculLocator.setVisible(false);
+        selBtnCalculDistance.setVisible(false);
+
+    }
+
+    @FXML
+    private void setCalculLocator(){
+
+        selBtnCarnetTrafic.setVisible(false);
+        selBtnCalculLocator.setVisible(true);
+        selBtnCalculDistance.setVisible(false);
+
+    }
+
+    @FXML
+    private void setCalculDistance(){
+
+        selBtnCarnetTrafic.setVisible(false);
+        selBtnCalculLocator.setVisible(false);
+        selBtnCalculDistance.setVisible(true);
+
+    }
 }
